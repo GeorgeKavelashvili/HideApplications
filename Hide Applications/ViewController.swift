1fc35825f7f701798fe6143b76581e260a6342c9
GeoulMan <gkavelashvili2@gmail.com>
2023-01-09 17:41:01 +0400

    final project

---
+++ Hide Applications/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hide Applications
+//
+//  Created by George on 04.12.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
